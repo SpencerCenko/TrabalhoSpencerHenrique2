5cd441201e130b259b0a720116421f7b48329dfd
SpencerCenko <spencercenko@gmail.com>
2025-12-04 12:16:30 -0300

    Update login.py

--- login.py
+++ login.py
@@ -38,7 +38,6 @@ senha.place(height=20,width=150,y=70,x=100)
 
 
 
-cardapiob = tk.Button(janela, text="Cardapio", command=lambda: cardapio()).place(x=100, y=170)
 
 
 def cardapio():
@@ -78,16 +77,18 @@ def select():
     jLista = tk.Toplevel()#uma segunda tela
     jLista.title("Lista")#nome da tela
     jLista.geometry("400x250")#tamanho
-    cursor.execute("select nome,preco from cardapio")#Cogido do sql 
+    cursor.execute("select id,nome,preco from cardapio")#Cogido do sql 
     card = cursor.fetchall()#puxando dos os dados fornecidos pelo mysql
-    selec = ttk.Treeview(jLista,columns=("Nome","Preco"),show="headings")#criando uma treeview e definindo o nome das colunas
+    selec = ttk.Treeview(jLista,columns=("id","Nome","Preco"),show="headings")#criando uma treeview e definindo o nome das colunas
+    selec.column("id",width=10,minwidth=50)#definindo o tamanho da coluna
     selec.column("Nome",width=100,minwidth=50)#definindo o tamanho da coluna
     selec.column("Preco",width=100,minwidth=50)#definindo o tamanho da coluna
+    selec.heading("id",text="ID:")#definindo qual o texto que ira aparecer na coluna
     selec.heading("Nome",text="Nome:")#definindo qual o texto que ira aparecer na coluna
     selec.heading("Preco",text="Preço:")#definindo qual o texto que ira aparecer na coluna
     selec.pack()#terminando a treeview
-    for(n,p) in card:
-        selec.insert("","end",values=(n,p))
+    for(i,n,p) in card:
+        selec.insert("","end",values=(i,n,p))
 def prato(nome2, preco,id):
         nome2.delete(0, tk.END) #resetando nome2
         preco.delete(0, tk.END) #resetando preco
@@ -115,36 +116,41 @@ def log():
      
     query = "select nome from log where nome = '"+e1+"'"#funções sendo passadas para o mysql e sendo definidas dentro de uma variavel
     query2 = "select senha from log where nome = '"+e1+"'"#funções sendo passadas para o mysql e sendo definidas dentro de uma variavel
+    query3 = "select admin from log where nome='"+e1+"'"
     cursor.execute(query, e1)#usa o cursor para executar o comando e a variavel mais o e1 que e == nome.get()
     userNome = str(cursor.fetchone()).replace("(", "").replace(")", "").replace("]", "").replace("[", "").replace("'", "").replace(",", "")#ele subistitui os caracteres por caracteres vazios e o str trasforma o cursor.fetchone() em string pois ele vem como truple
     cursor.execute(query2, e1)#usa o cursor para executar o comando e a variavel mais o e2 que e == senha.get()
     userSenha = str(cursor.fetchone()).replace("(", "").replace("(", "").replace(")", "").replace("]", "").replace("[", "").replace("'", "").replace(",", "")#ele subistitui os caracteres por caracteres vazios e o str trasforma o cursor.fetchone() em string pois ele vem como truple 
+    cursor.execute(query3, e1)
+    userAdmin = str(cursor.fetchone()[0])
 
-    if(e1 == ""and e2 == ""):
+    if e1 == ""and e2 == "":
      messagebox.showerror("Erro",'Digite os valores')#messagebox importado par quando os dados estiverm em branco
-
-    if(e1 == userNome and e2 == userSenha): 
-        tk.Label(janela,text="Login efetuado com sucesso").place(x=100,y=200)
-        janela.destroy()#fecha a janela principal 
-        jsecundaria = tk.Tk()#definindo a janela 
-        jsecundaria.title("Principal")#Nome da janela 
-        jsecundaria.config(bg="#b5b5b5")
-        jsecundaria.geometry("400x250")#tamanho da janela
-        jsecundaria.resizable(0,0)#não permite que a janela seja maximizada
-        tk.Label(jsecundaria,text='Nome:',bg="#b5b5b5").place(x=9,y=41)#X e de um lado pro outro e y cima e baixo
-        tk.Label(jsecundaria,text='Preço:',bg="#b5b5b5").place(x=9,y=80)#X e de um lado pro outro e y cima e baixo
-        tk.Label(jsecundaria,text='ID:',bg="#b5b5b5").place(x=9,y=13)
-        id = tk.Entry(jsecundaria)
-        nome2=tk.Entry(jsecundaria)
-        preco=tk.Entry(jsecundaria)
-        id.place(width=50,x=50,y=13)
-        preco.place(width=150,x=50,y=80)
-        nome2.place(width=150,x=50,y=41)
-        tk.Button(jsecundaria,text="Procurar produto",command=lambda:prato(nome2,preco,id)).place(x=250,y=10)
-        tk.Button(jsecundaria,text='select',command=lambda:select()).place(x=10,y=130)#botao de select
-        tk.Button(jsecundaria,text='update', command=lambda: upd(nome2, preco,id)).place(x=60,y=130)#botao de update 
-        tk.Button(jsecundaria,text='insert', command=lambda: insert(nome2, preco)).place(x=120,y=130)#botao de insert
-        tk.Button(jsecundaria,text='delete',command=lambda: delete(nome2)).place(x=170,y=130)#botao de delet
+    if(e1 == userNome and e2 == userSenha):
+        if userAdmin == "1":
+            tk.Label(janela,text="Login efetuado com sucesso").place(x=100,y=200)
+            janela.destroy()#fecha a janela principal 
+            jsecundaria = tk.Tk()#definindo a janela 
+            jsecundaria.title("Principal")#Nome da janela 
+            jsecundaria.config(bg="#b5b5b5")
+            jsecundaria.geometry("400x250")#tamanho da janela
+            jsecundaria.resizable(0,0)#não permite que a janela seja maximizada
+            tk.Label(jsecundaria,text='Nome:',bg="#b5b5b5").place(x=9,y=41)#X e de um lado pro outro e y cima e baixo
+            tk.Label(jsecundaria,text='Preço:',bg="#b5b5b5").place(x=9,y=80)#X e de um lado pro outro e y cima e baixo
+            tk.Label(jsecundaria,text='ID:',bg="#b5b5b5").place(x=9,y=13)
+            id = tk.Entry(jsecundaria)
+            nome2=tk.Entry(jsecundaria)
+            preco=tk.Entry(jsecundaria)
+            id.place(width=50,x=50,y=13)
+            preco.place(width=150,x=50,y=80)
+            nome2.place(width=150,x=50,y=41)
+            tk.Button(jsecundaria,text="Procurar produto",command=lambda:prato(nome2,preco,id)).place(x=250,y=10)
+            tk.Button(jsecundaria,text='select',command=lambda:select()).place(x=10,y=130)#botao de select
+            tk.Button(jsecundaria,text='update', command=lambda: upd(nome2, preco,id)).place(x=60,y=130)#botao de update 
+            tk.Button(jsecundaria,text='insert', command=lambda: insert(nome2, preco)).place(x=120,y=130)#botao de insert
+            tk.Button(jsecundaria,text='delete',command=lambda: delete(nome2)).place(x=170,y=130)#botao de delet
+        elif userAdmin == "0":
+            cardapio()
     else:
         tk.Label(janela,text="nome/senha estao incorretos").place(x=100,y=200)#mostra quando os dados do usuarios estão incorretos
 botao = tk.Button(janela, text="Entrar",command=log).place(height=50,width=100,y=100,x=50)#botao que executa a ação
